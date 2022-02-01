60d8d60457e3fe4265b88108376a4daaf5d9e560
AzureAD\VíctorSanzMiguens <2021358@iesluisbraille.es>
2022-02-01 12:49:14 +0100

    version_2

---
+++ version_2.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 09:46:39 2022
+
+@author: VíctorSanzMiguens
+"""
+
+while True:
+        print("""
+    -------------------------------------------------------------------------------          
+        Dime, ¿qué quieres hacer?
+        
+        1) Sumar los dos números
+        2) Restar los dos números
+        3) Multiplicar los dos números
+        4) Dividir los dos números 
+        5) Cociente de los números
+        6) Resto de los números
+        7) Exponencial de los números
+        8) Area triangulo
+        9) Area cuadrado
+        10) Area rectángulo
+        11) Area círculo
+        12) Apagar 
+    -------------------------------------------------------------------------------    
+        
+        """)
+        opcion = int(input("Elige una opción: ") )     
+
+        if opcion == 1:            
+            print(" ")
+          
+            
+        elif opcion == 2:        
+            print("")
+            
+        elif opcion == 3:
+            print("")
+            
+        elif opcion==4:
+            print("")
+    
+        elif opcion == 5:
+            print("")
+               
+        elif opcion == 6:
+               print("")
+               
+        elif opcion == 7:
+            print("")
+            
+        elif opcion==8:
+            print("")
+            
+        elif opcion==9:
+            print("")
+            
+        elif opcion==10:
+            print("")
+
+            
+        elif opcion==11:
+            print("")
+           
+        elif opcion == 12:
+            print("")
+#solo menu de opciones sin break ni funciones
