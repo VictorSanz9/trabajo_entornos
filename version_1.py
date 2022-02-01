8897de61df97290ef7efd1596cddc82b70005324
AzureAD\VíctorSanzMiguens <2021358@iesluisbraille.es>
2022-02-01 12:39:42 +0100

    version_1

---
+++ version_1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 09:00:25 2022
+
+@author: VíctorSanzMiguens
+"""
+
+while True:
+        print("""
+    -------------------------------------------------------------------------------          
+        Dime, ¿qué quieres hacer?
+        
+        1) Sumar los dos números
+        2) Restar los dos números
+        3) Multiplicar los dos números
+        4) Dividir los dos números 
+        5) Cociente de los números
+        6) Resto de los números
+        7) Exponencial de los números
+        8) Area triangulo
+        9) Area cuadrado
+        10) Area rectángulo
+        11) Area círculo
+        12) Apagar 
+    -------------------------------------------------------------------------------    
+        
+        """)
